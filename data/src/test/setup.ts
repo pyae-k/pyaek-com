70dd6eed3ea463e223e729971a441d5f8fc0a975
pyaek <pyaek@icloud.com>
2026-07-21 04:38:36 +0700

    20260721_4_38

--- data/src/test/setup.ts
+++
@@ -1,5 +0,0 @@
-// Vitest global setup. Pure logic tests (formula translator, CTE builder,
-// global-setup generator, base-path, project migration, step buildSql) run in
-// the node environment and need no DOM. Add shared matchers/globals here as
-// the suite grows.
-export {};
